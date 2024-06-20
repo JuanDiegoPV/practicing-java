2310ba21ec59f46c90574fbc1832cd655c6bed69
Juan Diego Peña <juandiegopena@MacBook-Air-de-Luis.local>
2024-06-20 16:17:29 -0700

    Translate message into english

--- src/main/java/com/mycompany/aprendiendogeneral/AprendiendoGeneral.java
+++ src/main/java/com/mycompany/aprendiendogeneral/AprendiendoGeneral.java
@@ -6,6 +6,6 @@ package com.mycompany.aprendiendogeneral;
 public class AprendiendoGeneral {
 
     public static void main(String[] args) {
-        System.out.println("Hola mundo!");
+        System.out.println("Hello world!");
     }
 }
