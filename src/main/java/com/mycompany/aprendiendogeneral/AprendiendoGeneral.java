38216f52d42af7faa5f4c2374b03038aa8ef54aa
Juan Diego Peña <juandiegopena@MacBook-Air-de-Luis.local>
2024-06-20 16:14:47 -0700

    initial commit

---
+++ src/main/java/com/mycompany/aprendiendogeneral/AprendiendoGeneral.java
@@ -0,0 +1,11 @@
+
+
+package com.mycompany.aprendiendogeneral;
+
+
+public class AprendiendoGeneral {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!");
+    }
+}
