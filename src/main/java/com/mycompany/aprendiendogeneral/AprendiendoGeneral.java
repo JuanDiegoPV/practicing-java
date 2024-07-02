348687732adf51639c47ec446e791fe482b3a9b8
Juan Diego <juandiegopb05@hotmail.com>
2024-07-01 21:08:11 -0700

    add 1 general formula exercise

--- src/main/java/com/mycompany/aprendiendogeneral/AprendiendoGeneral.java
+++ src/main/java/com/mycompany/aprendiendogeneral/AprendiendoGeneral.java
@@ -6,6 +6,18 @@ package com.mycompany.aprendiendogeneral;
 public class AprendiendoGeneral {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
+        int a = 2;
+        int b = -3;
+        int c = -2;
+        
+        
+        
+        double upperDivision = Math.sqrt(Math.pow(b, 2) - (4 * a * c));
+        double lowerDivision = 2 * a;
+        double add_upperDivision = -b + upperDivision;
+        double result = add_upperDivision / lowerDivision; 
+        System.out.println(result);
+        
     }
 }
