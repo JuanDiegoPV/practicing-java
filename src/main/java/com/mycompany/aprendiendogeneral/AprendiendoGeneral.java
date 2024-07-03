95b9ae82ecb8b8f2264eb7b2d8273d543afb4dfe
Juan Diego <juandiegopb05@hotmail.com>
2024-07-02 19:25:35 -0700

    add more general formula exercises

--- src/main/java/com/mycompany/aprendiendogeneral/AprendiendoGeneral.java
+++ src/main/java/com/mycompany/aprendiendogeneral/AprendiendoGeneral.java
@@ -17,7 +17,27 @@ public class AprendiendoGeneral {
         double lowerDivision = 2 * a;
         double add_upperDivision = -b + upperDivision;
         double result = add_upperDivision / lowerDivision; 
-        System.out.println(result);
+        System.out.println("Result 1: " + result);
+        
+        int a1 = 2;
+        int b1 = -3;
+        int c1 = -2;
+        
+        double upperDivision1 = Math.sqrt(Math.pow(b1, 2) - (4 * a1 * c1));
+        double lowerDivision1 = 2 * a1;
+        double add_upperDivision1 = -b1 + upperDivision1;
+        double result1 = add_upperDivision1 / lowerDivision1;
+        System.out.println("Result 2: " + result1);
+        
+        int a2 = 3;
+        int b2 = 5;
+        int c2 = 2;
+        
+        double upperDivision2 = Math.sqrt(Math.pow(b2, 2) - (4 * a2 * c2));
+        double lowerDivision2 = 2 * a2;
+        double add_upperDivision2 = -b2 + upperDivision2;
+        double result2 = add_upperDivision2 / lowerDivision2;
+        System.out.println("Result 3: " + result2);
         
     }
 }
